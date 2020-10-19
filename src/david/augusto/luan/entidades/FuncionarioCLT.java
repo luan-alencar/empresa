32b121f9eeac11504c7e90ca9a3f38ac53632732
Luan-Augusto <luanalencar134@gmail.com>
2020-10-19 14:33:19 -0300

    adicionando comentarios em FuncionarioCLT

--- src/david/augusto/luan/entidades/FuncionarioCLT.java
+++ src/david/augusto/luan/entidades/FuncionarioCLT.java
@@ -1,10 +1,20 @@
 package david.augusto.luan.entidades;
 
+// um funcionario cm contrato a consolidação das leis do trabalho (CLT) , 
+// ou carteira assinada, possui um salario fixo.
 public class FuncionarioCLT extends Funcionario {
 	private double salario;
 
 	public FuncionarioCLT(String cpf, String nome, double salario) {
 		super(cpf, nome);
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
 	}
 
 }
