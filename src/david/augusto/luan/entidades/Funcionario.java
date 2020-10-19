67fe36640469d58290ad2f02cbd8abd00e99a787
Luan-Augusto <luanalencar134@gmail.com>
2020-10-19 14:27:09 -0300

    primeiro commit

---
+++ src/david/augusto/luan/entidades/Funcionario.java
@@ -0,0 +1,37 @@
+package david.augusto.luan.entidades;
+
+public abstract class Funcionario {
+	private String cpf;
+	private String nome;
+	private double salario;
+
+	public Funcionario(String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+}
---
+++ src/david/augusto/luan/entidades/FuncionarioCLT.java
@@ -0,0 +1,10 @@
+package david.augusto.luan.entidades;
+
+public class FuncionarioCLT extends Funcionario {
+	private double salario;
+
+	public FuncionarioCLT(String cpf, String nome, double salario) {
+		super(cpf, nome);
+	}
+
+}
